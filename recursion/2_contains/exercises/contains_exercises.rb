4dcea6cebe5b330ce1203466ea08744224e7d1b4
gbntb <228612152+gbntb@users.noreply.github.com>
2025-12-28 14:26:54 -0400

    Complete exercise 2_contains (with solution)

--- recursion/2_contains/exercises/contains_exercises.rb
+++ recursion/2_contains/exercises/contains_exercises.rb
@@ -5,4 +5,9 @@ def contains?(hash, search_value)
   # Examples:
   # contains?({ foo: { bar: "baz" } }, "baz") # true
   # contains?({ foo: { bar: "baz" } }, "egg") # false
+  values = hash.values
+  return true if values.include?(search_value)
+
+  nested_hashes = values.select { |value| value.is_a?(Hash) }
+  nested_hashes.any? { |nested_hash| contains?(nested_hash, search_value) }
 end
