63123929f5c035f4b375d98df3a0d40936632e60
gbntb <228612152+gbntb@users.noreply.github.com>
2025-12-28 14:23:11 -0400

    Complete exercise 1_factorial

--- recursion/1_factorial/exercises/factorial.rb
+++ recursion/1_factorial/exercises/factorial.rb
@@ -1,3 +1,7 @@
 def factorial(num)
   # write a recursive method for calculating the [factorial](https://simple.wikipedia.org/wiki/Factorial) of a number
+  return nil unless num >= 0
+  return 1 if num.zero?
+
+  return num * factorial(num - 1)
 end
