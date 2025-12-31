de3240d62f80b3e1e342d7dba1f491c3c1b82e6f
gbntb <228612152+gbntb@users.noreply.github.com>
2025-12-31 10:10:19 -0400

    Complete exercise 5_pascal (with solution)

--- recursion/5_pascal/exercises/pascal_exercises.rb
+++ recursion/5_pascal/exercises/pascal_exercises.rb
@@ -15,4 +15,11 @@ def pascal(row_number)
   # Your task is to create a *recursive* function, `pascal` - that will take an input `n` and output the `n`th pascal's row as an array of numbers.
   #
   # For example, `pascal(3)` should return `[1, 2, 1]`.
+  current_line = [1]
+  return current_line if row_number == 1
+
+  previous_line = pascal(row_number - 1) + [0]
+  previous_line.each_cons(2) { |a, b| current_line << a + b }
+
+  current_line
 end
